e827b13809b54f54fdd8e0d0a252d3d364f7b6c0
Балаєв Віталій <vitaliibalaiev@gmail.com>
2025-01-04 23:49:34 +0200

    Add Angular safe navigation operator to prevent accessing properties of undefined.

--- src/pipes/format-followers.pipe.ts
+++ src/pipes/format-followers.pipe.ts
@@ -5,7 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'formatFollowers'
 })
 export class FormatFollowersPipe implements PipeTransform {
-    transform(value: number): string {
+    transform(value: number | null | undefined): string {
+        if (value === null || value === undefined) {
+            return '0';
+        }
+
         if (value >= 1_000_000) {
             return `${(value / 1_000_000).toFixed(2)}M`;
         } else if (value >= 1_000) {
