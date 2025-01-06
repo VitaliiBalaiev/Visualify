0b5282180e9b9ebee9a5266f6cb1b8388e31dd56
Балаєв Віталій <vitaliibalaiev@gmail.com>
2025-01-07 01:24:02 +0200

    Fix share popup slight lag

--- src/share-popup/share-popup.component.ts
+++ src/share-popup/share-popup.component.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-share-popup',
+  selector: 'share-popup',
   imports: [],
   templateUrl: './share-popup.component.html',
   standalone: true,
