b0fc4dc4ca7bb569d8991f9417bb7ed1368a82c2
Балаєв Віталій <vitaliibalaiev@gmail.com>
2025-01-08 17:18:11 +0200

    Add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
 import {CommonModule} from '@angular/common';
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+    imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent{
+  
+  constructor(private router: Router) {}
+  
+  isHomePage(): boolean {
+    return this.router.url === '/';
+}
 }
